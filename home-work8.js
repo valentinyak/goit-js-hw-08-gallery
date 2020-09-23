2123e80eae0e7ed7c341ddae5c912d58460eb290
valentinyak <63512456+valentinyak@users.noreply.github.com>
2020-09-23 22:24:55 +0300

    Добавлет код необязательной части ДЗ

--- home-work8.js
+++ home-work8.js
@@ -6,14 +6,26 @@ const imgEl = document.querySelector('img.lightbox__image');
 const closeBtnEl = document.querySelector(
   'button[data-action="close-lightbox"]',
 );
+const overlayEl = document.querySelector('div.lightbox__overlay');
 let tagsString = '';
 
-images.forEach(makeTagsString);
+images.forEach(createTagsString);
 gallaryListEl.insertAdjacentHTML('afterbegin', tagsString);
+
 gallaryListEl.addEventListener('click', onGallaryClick);
 closeBtnEl.addEventListener('click', onBtnCloseClick);
+overlayEl.addEventListener('click', onBtnCloseClick);
+window.addEventListener('keydown', event => {
+  if (event.code === 'Escape') {
+    onBtnCloseClick();
+  } else if (event.code === 'ArrowRight') {
+    onArrowRightPress();
+  } else if (event.code === 'ArrowLeft') {
+    onArrowLeftPress();
+  } else return;
+});
 
-function makeTagsString({ preview, original, description }) {
+function createTagsString({ preview, original, description }) {
   tagsString += `<li class="gallery__item">
     <a class="gallery__link" href="${original}">
     <img
@@ -24,19 +36,44 @@ function makeTagsString({ preview, original, description }) {
     />
     </a>
 </li>`;
-  //
 }
 
 function onGallaryClick(event) {
-  if (event.target.nodeName === 'IMG') {
-    modalEl.classList.add('is-open');
-    imgEl.src = `${event.target.dataset.source}`;
-    console.log(event.target.dataset.source);
-    console.log(imgEl.src);
+  if (event.target.nodeName !== 'IMG') {
+    return;
   }
+  modalEl.classList.add('is-open');
+  imgEl.src = `${event.target.dataset.source}`;
+  event.preventDefault();
 }
 
 function onBtnCloseClick() {
   modalEl.classList.remove('is-open');
   imgEl.src = '';
+}
+
+function onArrowLeftPress() {
+  if (
+    imgEl.src ===
+    gallaryListEl.firstElementChild.firstElementChild.firstElementChild.dataset
+      .source
+  ) {
+    imgEl.src = `${gallaryListEl.lastElementChild.firstElementChild.firstElementChild.dataset.source}`;
+  } else {
+    const thisEl = document.querySelector(`[data-source="${imgEl.src}"]`);
+    imgEl.src = `${thisEl.parentElement.parentElement.previousElementSibling.firstElementChild.firstElementChild.dataset.source}`;
+  }
+}
+
+function onArrowRightPress() {
+  if (
+    imgEl.src ===
+    gallaryListEl.lastElementChild.firstElementChild.firstElementChild.dataset
+      .source
+  ) {
+    imgEl.src = `${gallaryListEl.firstElementChild.firstElementChild.firstElementChild.dataset.source}`;
+  } else {
+    const thisEl = document.querySelector(`[data-source="${imgEl.src}"]`);
+    imgEl.src = `${thisEl.parentElement.parentElement.nextElementSibling.firstElementChild.firstElementChild.dataset.source}`;
+  }
 }
